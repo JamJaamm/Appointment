1021d8daec507a6f45fe7b316bdb230671d3a2c4
Owodunni Jamaldeen <mobongjubola@gmail.com>
2026-05-05 15:45:38 +0100

    Update ALLOWED_HOSTS for production settings

--- doctors_appointment/settings.py
+++ doctors_appointment/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', 'presentfilmmayor.pythonanywhere.com', '*']
+ALLOWED_HOSTS = ['*', '.onrender.com']
 
 
 # Application definition
