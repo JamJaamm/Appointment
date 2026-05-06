d6dc1412b57374072ff9300d98d3fbb17957a8b5
Owodunni Jamaldeen <mobongjubola@gmail.com>
2026-05-06 13:59:21 +0100

    Update settings.py

--- doctors_appointment/settings.py
+++ doctors_appointment/settings.py
@@ -17,7 +17,7 @@ import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent 
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev-fallback-key")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-fallback-key")
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.onrender.com']
 
 
 # Application definition
