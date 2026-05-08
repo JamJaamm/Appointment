06733fc03f7d2a89056dececaf49e0af4646b308
Owodunni Jamaldeen <mobongjubola@gmail.com>
2026-05-08 15:29:39 +0100

    Update settings.py

--- doctors_appointment/settings.py
+++ doctors_appointment/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','.onrender.com','.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
