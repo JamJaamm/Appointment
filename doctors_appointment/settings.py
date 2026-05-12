8f0e02f55b0fb6e9f381685c002be5169e1e4963
Owodunni Jamaldeen <mobongjubola@gmail.com>
2026-05-12 13:16:52 +0100

    Update settings.py

--- doctors_appointment/settings.py
+++ doctors_appointment/settings.py
@@ -17,8 +17,7 @@ import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent 
-SECRET_KEY = "test-secret-key"
-# SECRET_KEY = os.environ.get("SECRET_KEY","fallback-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY","fallback-secret-key")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
